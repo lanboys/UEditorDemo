5d9a1cc4066ec00e549bec13a1bfc7ca2d47408c
lanboys <lan_bing2013@163.com>
2018-12-28 13:52:13 +0800

    自定义上传图片接口

--- web/_src/plugins/simpleupload.js
+++ web/_src/plugins/simpleupload.js
@@ -79,6 +79,7 @@ UE.plugin.register('simpleupload', function (){
                             showErrorLoader && showErrorLoader(json.state);
                         }
                     }catch(er){
+                        console.log(er)
                         showErrorLoader && showErrorLoader(me.getLang('simpleupload.loadError'));
                     }
                     form.reset();
